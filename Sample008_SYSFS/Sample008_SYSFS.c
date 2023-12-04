ca9f34d130001d2bd42549174de25817d7145e59
HolmesChang <cct19840608@gmail.com>
2023-12-04 10:23:39 +0800

    1. Updating: In `Sample008_SYSFS.c`, do some house-keeping, and add error handling code.

--- Sample008_SYSFS/Sample008_SYSFS.c
+++ Sample008_SYSFS/Sample008_SYSFS.c
@@ -18,23 +18,72 @@ MODULE_AUTHOR("HolmesChang");
 MODULE_VERSION("V001P001");
 MODULE_DESCRIPTION("Sample008_SYSFS");
 
-// Step01: Creation Of Character Device Number //Registration Of Character Device
-#define PCD_DEVNO_NAME	"PCD_DEVNO_NAME"
-#define PCD_CLASS_NAME	"PCD_CLASS"
-#define PCD_DEVICE_NAME	"PCD_DEVICE"
+#undef		pr_fmt
+#define		pr_fmt(fmt) "%s: " fmt,__func__
 
-dev_t pcd_dev_no;
-
-// Step02: Registration Of Character Device To Linux VFS
-// Step02_01: Initialization Of Character Device With File Operations
-struct cdev pcd_cdev;
-struct file_operations pcd_fops;
-
-struct class * pcd_class;
-struct device * pcd_device;
+#define MY_DEV_NO_NAME	"MY_DEV_NO_NAME"
+#define MY_CLASS_NAME	"MY_CLASS"
+#define MY_DEVICE_NAME	"MY_DEVICE"
 
 static uint32_t pcd_buf_size = 128;
 
+/*
+	Declaration Of Device Number
+*/
+dev_t pcd_dev_no;
+
+/*
+	Declaration AND Initialization Of Variable AND Function For:
+		Character Device File In /dev VFS
+		Class In /sys VFS
+*/
+int my_cdev_fops_open (struct inode * inode, struct file * filp) {
+	pr_info("Begin\n");
+	
+	pr_info("End\n");
+	
+	return 0;
+}
+
+int my_cdev_fops_release (struct inode * inode, struct file * filp) {
+	pr_info("Begin\n");
+	
+	pr_info("End\n");
+	
+	return 0;
+}
+
+ssize_t my_cdev_fops_write (struct file * filp, const char __user * ubuf, size_t count, loff_t * fpos) {
+	pr_info("Begin\n");
+	
+	pr_info("End\n");
+	
+	return count;
+}
+
+ssize_t my_cdev_fops_read (struct file * filp, char __user * ubuf, size_t count, loff_t * fpos) {
+	pr_info("Begin\n");
+	
+	pr_info("End\n");
+	
+	return 0;
+}
+
+struct cdev my_cdev;
+struct file_operations my_cdev_fops = {
+	.owner		= THIS_MODULE,
+	.open		= my_cdev_fops_open,
+	.release	= my_cdev_fops_release,
+	.write		= my_cdev_fops_write,
+	.read		= my_cdev_fops_read
+};
+
+struct class * my_class;
+struct device * my_device;
+
+/*
+	Variable AND Function For /sys VFS
+*/
 ssize_t pcd_buf_size_show (struct device * dev, struct device_attribute * attr, char * buf) {
 	pr_info("Begin\n");
 	
@@ -51,46 +100,78 @@ ssize_t pcd_buf_size_store (struct device * dev, struct device_attribute * attr,
 
 DEVICE_ATTR(pcd_buf_size, S_IRUGO | S_IWUSR, pcd_buf_size_show, pcd_buf_size_store);
 
-static int __init PCD_Init (void) {
+static int __init Sample008_SYSFS_Init (void) {
 	int ret;
 	
-	printk("%s: Device Driver Initialization\n", __func__);
+	pr_info("Begin\n");
 	
-	// Step01: Creation Of Character Device Number //Registration Of Character Device
-	alloc_chrdev_region(&pcd_dev_no, 0, 1, PCD_DEVNO_NAME);
+	ret = alloc_chrdev_region(&pcd_dev_no, 0, 1, MY_DEV_NO_NAME);
+	if (ret) {
+		pr_err("alloc_chrdev_region(&pcd_dev_no, 0, 1, MY_DEV_NO_NAME): NG");
+		
+		return ret;
+	}
 	
-	// Step02: Registration Of Character Device To Linux VFS
-	// Step02_01: Initialization Of Character Device With File Operaitons
-	cdev_init(&pcd_cdev, &pcd_fops);
-	pcd_cdev.owner = THIS_MODULE;
+	cdev_init(&my_cdev, &my_cdev_fops);
+	my_cdev.owner = THIS_MODULE;
 	
-	// Step02_02: Adding Character Device To Linux VFS
-	cdev_add(&pcd_cdev, pcd_dev_no, 1);
+	ret = cdev_add(&my_cdev, pcd_dev_no, 1);
+	if (ret) {
+		pr_err("cdev_add(&my_cdev, pcd_dev_no, 1): NG");
+		
+		goto err_cdev_add;
+	}
 	
-	pcd_class = class_create(THIS_MODULE, PCD_CLASS_NAME);
+	my_class = class_create(THIS_MODULE, MY_CLASS_NAME);
+	if (IS_ERR(my_class)) {
+		pr_err("class_create(THIS_MODULE, MY_CLASS_NAME): NG\n");
+		
+		ret = PTR_ERR(my_class);
+		
+		goto err_class_create;
+	}
 	
-	pcd_device = device_create(pcd_class, NULL, pcd_dev_no, NULL, PCD_DEVICE_NAME);
+	my_device = device_create(my_class, NULL, pcd_dev_no, NULL, MY_DEVICE_NAME);
+	if (IS_ERR(my_device)) {
+		pr_err("device_create(my_class, NULL, pcd_dev_no, NULL, MY_DEVICE_NAME): NG\n");
+		
+		ret = PTR_ERR(my_device);
+		
+		goto err_class_create;
+	}
 	
-	// Step03: Adding Device Attribute To SYSFS
-	ret = sysfs_create_file(&pcd_device->kobj, &dev_attr_pcd_buf_size.attr);
+	ret = sysfs_create_file(&my_device->kobj, &dev_attr_pcd_buf_size.attr);
 	if (ret != 0) {
 		pr_err("Creating SYSFS File: NG\n");
 	}
 	
+	pr_info("End\n");
+	
 	return 0;
+	
+err_device_create:
+	class_destroy(my_class);
+err_class_create:
+	cdev_del(&my_cdev);
+err_cdev_add:
+	unregister_chrdev_region(pcd_dev_no, 1);
+	
+	return ret;
 }
 
-static void __exit PCD_Exit (void) {
-	printk("%s: Device Driver Finalization\n", __func__);
+static void __exit Sample008_SYSFS_Exit (void) {
+	pr_info("Begin\n");
 	
-	device_destroy(pcd_class, pcd_dev_no);
+	device_destroy(my_class, pcd_dev_no);
 	
-	class_destroy(pcd_class);
+	class_destroy(my_class);
 	
-	cdev_del(&pcd_cdev);
+	cdev_del(&my_cdev);
 	
 	unregister_chrdev_region(pcd_dev_no, 1);
+	
+	pr_info("End\n");
 }
 
-module_init(PCD_Init);
-module_exit(PCD_Exit);
+module_init(Sample008_SYSFS_Init);
+module_exit(Sample008_SYSFS_Exit);
